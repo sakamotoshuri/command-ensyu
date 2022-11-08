97379ad62d9f96a4107da2f192404aa99d7ef856
sakamoto <s.sakamoto.sys22@morijyobi.ac.jp>
2022-11-08 13:44:22 +0900

    "正しく完了"

--- Main.java
+++ Main.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
-
 public class Main {
-    public static void main(String args[] ) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("身長(m)：");
-        double height = scanner.nextDouble();
-        System.out.print("体重(kg)：");
-        double weight = scanner.nextDouble();
-
-        double bmi = weight / (height * height);
-        double a = ((double)Math.round(bmi * 100))/100;
-        System.out.print("BMI："+a);
-    }
+	
+	public static void main(String[] args) {
+	 Scanner scan = new Scanner(System.in);
+	 for(int i=0; i<2;i++){
+                System.out.println("身長");
+                double height = scan.nextDouble();
+                System.out.println("体重");
+                double weight = scan.nextDouble();
+		double height2 = height / 100.0;
+                double BMI = weight / Math.pow(height,2);
+                System.out.println((double)Math.round(BMI * 100)/100);
+	 }
+                scan.close();
+		
+	}
 }
